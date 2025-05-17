ffd2e54091b2afa75bf0f8c62dafa0ce9681caaf
Poupoune Dt <poupoune62d@gmail.com>
2025-05-17 06:23:06 +0000

    jaimerai que tu retire la page dacceuil pour arriver directement a la page de connexions

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: false, // Set to true if this is a permanent change
+      },
+    ]
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
